93454a30450848e853287bc5240d19f9529b02c8
caru-ini <caru17122@gmail.com>
2024-06-20 13:47:58 +0900

    feat: configure d1 and drizzleorm

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,14 @@
+import { todos } from '@/db/schema';
 import { getRequestContext } from '@cloudflare/next-on-pages';
+import assert from 'assert';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { Hono } from 'hono';
 
 export const runtime = 'edge';
 
 type Bindings = {
-  MY_DB: D1Database;
+  DB: D1Database;
 };
 
 export const handle = (app: Hono<{ Bindings: Bindings }>) => (req: Request) => {
@@ -14,17 +18,38 @@ export const handle = (app: Hono<{ Bindings: Bindings }>) => (req: Request) => {
 
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
 
-// const routes = app
-//   .get('/posts', async (c) => {
-//     const db = drizzle(c.env.MY_DB);
-//     const results = await db.select().from(posts).all();
-//     return c.json(results);
-//   })
-//   .get('/tags', async (c) => {
-//     const db = drizzle(c.env.MY_DB);
-//     const results = await db.select().from(tags).all();
-//     return c.json(results);
-//   });
+const routes = app
+  .get('/todos', async (c) => {
+    const db = drizzle(c.env.DB);
+    const results = await db.select().from(todos).all();
+    return c.json(results);
+  })
+  .post('/todos', async (c) => {
+    const params = await c.req.json<typeof todos.$inferSelect>();
+    const db = drizzle(c.env.DB);
+    const result = await db.insert(todos).values(params).execute();
+    return c.json(result);
+  })
+  .put('/todos/:id', async (c) => {
+    const id = parseInt(c.req.param('id'));
+    assert(!isNaN(id));
+    const params = await c.req.json<typeof todos.$inferSelect>();
+    const db = drizzle(c.env.DB);
+    const result = await db
+      .update(todos)
+      .set({ text: params.text, done: params.done })
+      .where(eq(todos.id, id))
+      .execute();
+    return c.json(result);
+  })
+  .delete('/todos/:id', async (c) => {
+    const id = parseInt(c.req.param('id'));
+    assert(!isNaN(id));
+    const db = drizzle(c.env.DB);
+    const result = await db.delete(todos).where(eq(todos.id, id)).execute();
+    return c.json(result);
+  });
+
 export type AppType = typeof routes;
 
 export const GET = handle(app);
