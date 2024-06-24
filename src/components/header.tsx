b0af0d0fcb42ddeff4d5b6425812eb6aac87a237
caru-ini <caru17122@gmail.com>
2024-06-24 18:16:29 +0900

    refactor: Update ToggleTheme component in header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className='sticky top-0 z-50 flex h-[70px] w-full items-center justify-between border-b-2  border-border p-4 backdrop-blur-xl'>
       <h1 className='text-2xl'>Hono + R2 Todo App</h1>
       <div className='flex items-center space-x-4'>
-        <ToggleTheme></ToggleTheme>
+        <ToggleTheme />
       </div>
     </header>
   );
