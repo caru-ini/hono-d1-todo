a899a4e3344b9b6f415c5200673a1469e4876ba3
caru-ini <caru17122@gmail.com>
2024-06-24 18:17:17 +0900

    fix: Allow some HTTP operations

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -55,3 +55,9 @@ const routes = app
 export type AppType = typeof routes;
 
 export const GET = _handle(app);
+
+export const POST = _handle(app);
+
+export const PUT = _handle(app);
+
+export const DELETE = _handle(app);
