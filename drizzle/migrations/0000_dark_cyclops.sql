cb3c2d18b5afe738f5bd963219a2727d96b0b6fe
caru-ini <caru17122@gmail.com>
2024-06-25 14:49:48 +0900

    feat: Update schema

--- drizzle/migrations/0000_dark_cyclops.sql
+++ drizzle/migrations/0000_dark_cyclops.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `todos` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`text` text,
-	`done` integer
+	`done` integer DEFAULT false
 );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,5 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const todos = sqliteTable('todos', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   text: text('text'),
-  done: integer('done', { mode: 'boolean' }),
+  done: integer('done', { mode: 'boolean' }).default(false),
 });
