58cd9ff580d1f6d351932a7af4df0fab9ea44e85
caru-ini <caru17122@gmail.com>
2024-06-25 13:55:34 +0900

    fix: Fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex h-[calc(100lvh-70px)] w-full flex-col items-center'>
       <div className='flex flex-col items-center space-y-4 p-10'>
-        <h1 className='text-4xl'>Hono + R2 Todo App</h1>
+        <h1 className='text-4xl'>Hono + D1 Todo App</h1>
         <TodoList />
       </div>
     </main>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import ToggleTheme from './toggletheme';
 export const Header = () => {
   return (
     <header className='sticky top-0 z-50 flex h-[70px] w-full items-center justify-between border-b-2  border-border p-4 backdrop-blur-xl'>
-      <h1 className='text-2xl'>Hono + R2 Todo App</h1>
+      <h1 className='text-2xl'>Hono + D1 Todo App</h1>
       <div className='flex items-center space-x-4'>
         <ToggleTheme />
       </div>
