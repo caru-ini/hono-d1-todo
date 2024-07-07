0cd90f85cf361d36498401ce4390111f16521ded
caru-ini <caru17122@gmail.com>
2024-07-07 13:18:40 +0900

    refactor: move zod definitions to top

--- src/app/api/[[...route]]/todo.ts
+++ src/app/api/[[...route]]/todo.ts
@@ -6,45 +6,36 @@ import { z } from 'zod';
 
 import type { Env } from './route';
 
+const schemas = {
+  createTodo: z.object({
+    text: z.string(),
+  }),
+  updateTodo: z.object({
+    text: z.string().optional(),
+    done: z.boolean(),
+  }),
+};
+
 export const todo = new Hono<Env>()
   .get('/', async (c) => {
     const results = await c.var.db.select().from(todos).all();
     return c.json(results);
   })
-  .post(
-    '/',
-    zValidator(
-      'json',
-      z.object({
-        text: z.string(),
-      }),
-    ),
-    async (c) => {
-      const params = await c.req.json<typeof todos.$inferSelect>();
-      const result = await c.var.db.insert(todos).values(params).execute();
-      return c.json(result);
-    },
-  )
-  .put(
-    '/:id',
-    zValidator(
-      'json',
-      z.object({
-        text: z.string().optional(),
-        done: z.boolean(),
-      }),
-    ),
-    async (c) => {
-      const id = parseInt(c.req.param('id'));
-      const params = await c.req.json<typeof todos.$inferSelect>();
-      const result = await c.var.db
-        .update(todos)
-        .set({ text: params.text, done: params.done })
-        .where(eq(todos.id, id))
-        .execute();
-      return c.json(result);
-    },
-  )
+  .post('/', zValidator('json', schemas.createTodo), async (c) => {
+    const params = await c.req.json<typeof todos.$inferSelect>();
+    const result = await c.var.db.insert(todos).values(params).execute();
+    return c.json(result);
+  })
+  .put('/:id', zValidator('json', schemas.updateTodo), async (c) => {
+    const id = parseInt(c.req.param('id'));
+    const params = await c.req.json<typeof todos.$inferSelect>();
+    const result = await c.var.db
+      .update(todos)
+      .set({ text: params.text, done: params.done })
+      .where(eq(todos.id, id))
+      .execute();
+    return c.json(result);
+  })
   .delete('/:id', async (c) => {
     const id = parseInt(c.req.param('id'));
     const result = await c.var.db.delete(todos).where(eq(todos.id, id)).execute();
