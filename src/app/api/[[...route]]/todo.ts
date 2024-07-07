6ac8b139d2722c0c64a97e5816fc92de0c9c6345
caru-ini <caru17122@gmail.com>
2024-07-07 16:20:46 +0900

    feat: use drizzle-zod to validate

--- src/app/api/[[...route]]/todo.ts
+++ src/app/api/[[...route]]/todo.ts
@@ -1,34 +1,26 @@
 import { todos } from '@/db/schema';
 import { zValidator } from '@hono/zod-validator';
 import { eq } from 'drizzle-orm';
+import { createInsertSchema } from 'drizzle-zod';
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
-import { z } from 'zod';
 
 import type { Env } from './app';
 
-const schemas = {
-  createTodo: z.object({
-    text: z.string(),
-  }),
-  updateTodo: z.object({
-    text: z.string().optional(),
-    done: z.boolean(),
-  }),
-};
+const schema = createInsertSchema(todos);
 
 export const todo = new Hono<Env>()
   .get('/', async (c) => {
     const results = await c.var.db.select().from(todos).all();
     return c.json(results);
   })
-  .post('/', zValidator('json', schemas.createTodo), async (c) => {
+  .post('/', zValidator('json', schema), async (c) => {
     const params = await c.req.json<typeof todos.$inferSelect>();
     const result = await c.var.db.insert(todos).values(params).execute();
     if (!result.success) throw new HTTPException(500, { message: 'Failed to create todo' });
     return c.json(params);
   })
-  .put('/:id', zValidator('json', schemas.updateTodo), async (c) => {
+  .put('/:id', zValidator('json', schema), async (c) => {
     const id = parseInt(c.req.param('id'));
     const params = await c.req.json<typeof todos.$inferSelect>();
     const result = await c.var.db
